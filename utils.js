fd725482f199ca54131fa29c111acc6338d43b54
AlenaYanova <leonayanov@gmail.com>
2018-07-01 14:24:59 +0300

    fixed

--- utils.js
+++ utils.js
@@ -1,12 +1,11 @@
 function splitAndMerge(string, separator) {
-    const words_arr = string.split(' ');
-    let symbol_arr = [];
-    for (let i=0; i < words_arr.length; i++)
-        symbol_arr.push(words_arr[i].split('').join(separator))
-    return symbol_arr.join(separator);
+    return string
+        .split(' ')
+        .map((word) => {return word.split('').join(separator)})
+        .join(separator);
 }
 
-// console.log (splitAndMerge('Hello world!', ' '));
+// console.log (splitAndMerge('Hello world!', '/'));
 
 function convert(hash) {
     let arr = [];
@@ -21,9 +20,9 @@ function convert(hash) {
 function toCamelCase(string) {
     const words_arr = string.split(/[-_]/);
     let camel_str = '';
-    if (words_arr.length > 1) {
+    if (words_arr.length > 0) {
         camel_str = words_arr[0];
-        for (let i=1; i < words_arr.length; i++) {
+        for (let i = 1; i < words_arr.length; i++) {
             if (words_arr[i].length > 1)
                 camel_str += words_arr[i][0].toUpperCase() + words_arr[i].substr(1);
             else if (words_arr[i].length === 1)
@@ -33,11 +32,11 @@ function toCamelCase(string) {
     return camel_str;
 }
 
-// console.log(toCamelCase('The_stealth_warrior_a'));
+// console.log(toCamelCase('--THe-_waRRior__A--'));
 
 function reverseEachWord(string) {
     let res_str = '';
-    for (let i=0; i<string.length; i++){
+    for (let i = 0; i < string.length; i++){
         if (string[i] === ' ')
             res_str += string[i];
         else {
@@ -56,14 +55,14 @@ function reverseEachWord(string) {
 
 function stringExpansion(string) {
     let res_string = '';
-    for (let i=0; i<string.length; i++){
-        if (string[i]>='0' && string[i]<='9'){
-            if ((string[i+1]>='a' && string[i+1]<='z') || (string[i+1]>='A' && string[i+1]<='Z')) {
+    for (let i = 0; i < string.length; i++){
+        if (string[i] >= '0' && string[i] <= '9'){
+            if ((string[i+1] >= 'a' && string[i+1] <= 'z') || (string[i+1] >= 'A' && string[i+1] <= 'Z')) {
                 res_string += string[i + 1].repeat(string[i].valueOf());
                 ++i;
             }
         }
-        else if ((string[i]>='a' && string[i]<='z') || (string[i]>='A' && string[i]<='Z'))
+        else if ((string[i] >= 'a' && string[i] <= 'z') || (string[i] >= 'A' && string[i] <= 'Z'))
             res_string += string[i];
     }
     return res_string;
